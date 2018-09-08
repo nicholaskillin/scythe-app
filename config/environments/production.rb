67ebb0356567d9e81fa669446ab8d8e11cbe4f7f
Nick Killin <nicholaskillin@gmail.com>
2018-09-08 12:50:15 +0000

    update gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
