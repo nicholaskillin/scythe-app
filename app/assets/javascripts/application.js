afafcc8112904dbbb1f8df8f359029e6ca536fb0
Nick Killin <nicholaskillin@gmail.com>
2018-09-09 02:39:23 +0000

    fixing asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require rails-ujs
+//= require jquery3
 //= require turbolinks
 //= require_tree .
