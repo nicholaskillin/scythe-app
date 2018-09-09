0a110d6bd74daccb1bc14ce6f79cf09363ba6c45
Nick Killin <nicholaskillin@gmail.com>
2018-09-09 01:56:22 +0000

    increase asset version

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Scythe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.version = '1.1'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
