ad18b5a6078cac1c80e125af17be55ec93bb6f2c
Nick Killin <nicholaskillin@gmail.com>
2018-09-09 02:00:23 +0000

    maybe found it

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Scythe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.version = '1.1'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
