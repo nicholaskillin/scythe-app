a1faa33bb4a2bb0255e60d7af6c2dec544d418c6
Nick Killin <nicholaskillin@gmail.com>
2018-09-10 02:32:01 +0000

    got rid of submit buttons

--- app/assets/javascripts/homenew.js
+++ app/assets/javascripts/homenew.js
@@ -8,32 +8,38 @@ window.onload = function () {
   let playerOne = document.getElementById('player-one');
   let playerOneText = document.getElementById('player-one-text');
   let playerOneMat = document.getElementById('player-one-player-mat');
-  let playerOnePopButton = document.getElementById('player-one-pop-button');
+  let playerOnePop = document.getElementById('player-one-pop');
+  let playerOneStars = document.getElementById('player-one-stars');
   let gameStartedDivTwo = document.getElementById('game-started-2');
   let playerTwo = document.getElementById('player-two');
   let playerTwoText = document.getElementById('player-two-text');
   let playerTwoMat = document.getElementById('player-two-player-mat');
-  let playerTwoPopButton = document.getElementById('player-two-pop-button');
+  let playerTwoPop = document.getElementById('player-two-pop');
   let gameStartedDivThree = document.getElementById('game-started-3');
   let playerThree = document.getElementById('player-three');
   let playerThreeText = document.getElementById('player-three-text');
   let playerThreeMat = document.getElementById('player-three-player-mat');
+  let playerThreePop = document.getElementById('player-three-pop');
   let gameStartedDivFour = document.getElementById('game-started-4');
   let playerFour = document.getElementById('player-four');
   let playerFourText = document.getElementById('player-four-text');
   let playerFourMat = document.getElementById('player-four-player-mat');
+  let playerFourPop = document.getElementById('player-four-pop');
   let gameStartedDivFive = document.getElementById('game-started-5');
   let playerFive = document.getElementById('player-five');
   let playerFiveText = document.getElementById('player-five-text');
   let playerFiveMat = document.getElementById('player-five-player-mat');
+  let playerFivePop = document.getElementById('player-five-pop');
   let gameStartedDivSix = document.getElementById('game-started-6');
   let playerSix = document.getElementById('player-six');
   let playerSixText = document.getElementById('player-six-text');
   let playerSixMat = document.getElementById('player-six-player-mat');
+  let playerSixPop = document.getElementById('player-six-pop');
   let gameStartedDivSeven = document.getElementById('game-started-7');
   let playerSeven = document.getElementById('player-seven');
   let playerSevenText = document.getElementById('player-seven-text');
   let playerSevenMat = document.getElementById('player-Seven-player-mat');
+  let playerSevenPop = document.getElementById('player-seven-pop');
   let invaders = false;
   let gameStarted = false
 
@@ -54,7 +60,8 @@ function addPlayer(){
     playerNumber: players.length + 1,
     faction: factions.pop(),
     mat: playerMats.pop(),
-    popularity: 0
+    popularity: 0,
+    stars: 0
   };
   players.push(newPlayer);
 }
@@ -119,15 +126,58 @@ startGameButton.addEventListener('click', function() {
   gameStartedDivSeven.style.display = "block";
 });
 
-playerOnePopButton.addEventListener('click', function() {
-  let playerOneNewPop = document.getElementById('player-one-pop').value;
-  players[0].popularity = playerOneNewPop;
+// Update Player Popularity
+
+playerOnePop.addEventListener('input', function() {
+  let newPlayerPop = document.getElementById('player-one-pop').value;
+  players[0].popularity = newPlayerPop;
   console.log(players);
 });
 
-playerTwoPopButton.addEventListener('click', function() {
-  let playerTwoNewPop = document.getElementById('player-two-pop').value;
-  players[1].popularity = playerTwoNewPop;
+playerTwoPop.addEventListener('input', function() {
+  let newPlayerPop = document.getElementById('player-two-pop').value;
+  players[1].popularity = newPlayerPop;
+  console.log(players);
+});
+
+playerThreePop.addEventListener('input', function() {
+  let newPlayerPop = document.getElementById('player-three-pop').value;
+  players[2].popularity = newPlayerPop;
+  console.log(players);
+});
+
+playerFourPop.addEventListener('input', function() {
+  let newPlayerPop = document.getElementById('player-four-pop').value;
+  players[3].popularity = newPlayerPop;
+  console.log(players);
+});
+
+playerFivePop.addEventListener('input', function() {
+  let newPlayerPop = document.getElementById('player-five-pop').value;
+  players[4].popularity = newPlayerPop;
+  console.log(players);
+});
+
+playerSixPop.addEventListener('input', function() {
+  let newPlayerPop = document.getElementById('player-six-pop').value;
+  players[5].popularity = newPlayerPop;
+  console.log(players);
+});
+
+playerSevenPop.addEventListener('input', function() {
+  let newPlayerPop = document.getElementById('player-seven-pop').value;
+  players[6].popularity = newPlayerPop;
+  console.log(players);
+});
+
+// Update Player Stars
+
+playerOneStars.addEventListener('input', function() {
+  let newPlayerStars = document.getElementById('player-one-stars').value;
+  players[0].stars = newPlayerStars;
+  let playerStarsScore = players[0].stars * players[0].popularity;
+  let playerStarsScoreDiv = document.getElementById('player-one-stars-score');
+  playerStarsScoreDiv.innerHTML = playerStarsScore;
   console.log(players);
 });
 
